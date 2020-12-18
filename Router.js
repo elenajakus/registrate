b667757d8ad923453b7982add06eef962bf6d4db
elenajakus <48527965+elenajakus@users.noreply.github.com>
2020-12-18 08:54:40 +0100

    Add files via upload

---
+++ Router.js
@@ -0,0 +1,115 @@
+class Router{
+    constructor(app, db){
+        this.login(app, db);
+        this.logout(app, db);
+        this.isLoggedIn(app, db);
+
+    }
+
+    login(app, db){
+        app.post('login', (req, res) => {
+            let username = req.body.username;
+            let password = req.body.password;
+
+            username = username.toLowerCase();
+
+            if(username.length > 12 || password.length > 12 ){
+                res.json({
+                    succes: false, 
+                    msg: 'An error occured, please try again'
+
+                })
+                return;
+            }
+            let cols = [username];
+            db.query('SELECT * FROM user WHERE username = ? LIMIT 1', cols, (err, data, fields) =>{
+                if(err){
+                    res.json({
+                        success: false,
+                        msg: 'An error occured, please try again'
+
+                    })
+                    return;
+                }
+            
+                if(data && data.length ===1){
+                    bcrypt.compare(password, data[0].password, (bcrypt, verified) => {
+
+                        if(verified){
+                            req.session.userID = data[0].id;
+                        
+                        res.json({
+                            success: true,
+                            username: data[0].username 
+                        })
+                    return;
+                    
+                    }
+                    else{
+                        res.json({
+                            success: false,
+                            msg:'invalid password'
+                        })
+                    }
+                });
+            }else{
+                res.json({
+                    sucsess: false,
+                    msg:'User not found, please try again'
+                })
+            }
+                
+            });
+
+    });
+
+}
+    logout(app, db){
+        app.post('/logout', (req, res) =>{
+            if(req.session.userID){
+                req.session.destroy();
+                req.json({
+                    success: true
+                })
+                return true;
+            }else{
+                res.json({
+                    success:false
+                })
+                return false;
+            }
+        });
+
+    }
+    isLoggedIn(app, db){
+        app.post('/isLoggedIn', (req, res) =>{
+            if(req.session.userId){
+                let cols=[req.session.userId];
+                db.query('SELECT * FROM user WHERE id = ? LIMIt 1', cols, (err,data,fields) =>{
+
+                    if(data && data.length ===1){
+                        res.json({
+                            success: true,
+                            username: data[0].username
+                        })
+                        return true;
+                    }else{
+                        res.json({
+                            success: false
+                             })
+                        }
+
+                        });
+                
+                    }else{
+                 res.json({
+                success: false
+                 })
+                }
+        });
+
+    }
+
+}
+
+module.export = Router;
